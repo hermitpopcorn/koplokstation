b13efade5e680ec84a4515557f827fad9bb02e6e
hermitpopcorn <dani.hmng@gmail.com>
2016-12-22 10:19:32 +0700

    Add telolet sound to beepButton component because it's the freshest meme of 2016 as of writing this

--- imports/api/beeper/methods.js
+++ imports/api/beeper/methods.js
@@ -6,4 +6,8 @@ Meteor.methods({
   'beeper.beep'() {
     shell.exec("mpg123 " + Assets.absoluteFilePath("audio/chirp.mp3") + " &", {silent: true});
   },
+
+  'beeper.telolet'() {
+    shell.exec("mpg123 " + Assets.absoluteFilePath("audio/telolet.mp3") + " &", {silent: true});
+  },
 });
--- imports/ui/components/beep-button/beep-button.js
+++ imports/ui/components/beep-button/beep-button.js
@@ -1,9 +1,18 @@
 import './beep-button.html';
 
 Template.beepButton.events({
-  'click button'(event, instance) {
+  'click button.beep'(event, instance) {
     // beep the server when the button is pressed
     Meteor.call('beeper.beep', (error) => {
+      if (error) {
+        alert(error.error);
+      }
+    });
+  },
+
+  'click button.telolet'(event, instance) {
+    // om telolet because it's the freshest meme of 2016 as of writing this
+    Meteor.call('beeper.telolet', (error) => {
       if (error) {
         alert(error.error);
       }
