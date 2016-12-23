3832892a362e579cc0cd7bddcf8d52a530b2997b
hermitpopcorn <dani.hmng@gmail.com>
2016-12-23 14:14:50 +0700

    Remove basic auth (also bcrypt) because it's hard to integrate it with Flow Router

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,10 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // Add default user if the user list is empty
-  if(Meteor.call('users.isEmpty')) {
-    console.log("Users empty; attempting to insert default user...");
-    let attempt = Meteor.call('users.insert', "tiesto", "metatron");
-    console.log(attempt ? "User 'tiesto' with password 'metatron' inserted." : "Could not insert default user. Try inserting one manually.");
-  }
+
 });
